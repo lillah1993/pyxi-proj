1df8419e104970805cbd819ee387d9393706132c
ahmed <jj.93@outlook.com>
2022-03-15 17:26:47 +0100

    creating the tophead component styles

--- src/components/tophead/tophead.jsx
+++ src/components/tophead/tophead.jsx
@@ -4,36 +4,40 @@ import { ReactComponent as Settings } from "../../img/svg/settings.svg";
 import { ReactComponent as Power } from "../../img/svg/power-vector.svg";
 import "./tophead.style.scss";
 import Title from "../title/title";
-import Language from "../language/language";
+// import Language from "../language/language";
 
 const TopHead = () => {
-    const frtitles = ['ma situation', 'mes fonds', 'mes operarions' ,'simulateur', 'documenatation'];
-    const languages = ['fr','en'];
+    const frtitles = ['MA SITUATION', 'MES FONDS', 'MES OPERATION' ,'SIMULATEUR', 'DOCUMENTATION'];
+    // const languages = ['fr','en'];
     return(
     <div className="tophead">
-        <div className="logo">
-            <span></span>
-        </div>
-        <div className="languges">
-            {
-                languages.map((language,index)=> <Language key={index} language={language}/>)
-            }
-        </div>
+        <div className="container"> 
+                <div className="logo">
+                    <span className="first">CDG CAPITAL</span>
+                    <span className="second">GESTION</span>
+                    <span className="third">GROUPE CDG</span>
+                </div>
+                <div className="languages">
+                    {/* {
+                        languages.map((language,index)=> <Language key={index} language={language}/>)
+                    } */}
+                    <span className="lang">FR</span>
+                    <span className="line"/>
+                    <span className="lang">EN</span>
+                </div>
 
-        {
-            frtitles.map((title,index)=> <Title key={index} title={title}/>)
-        }
-        <div className="icons">
-            <div className="beil">
-                <Beil />
+                {
+                    frtitles.map((title,index)=> <Title key={index} title={title}/>)
+                }
+                <div className="icons">
+                    <Beil className="beil"/>
+                    <span className="badge"/>
+                    <span className="little-line"/>
+                    <Settings className="settings"/>
+                    <span className="big-line"/>
+                    <Power className="power"/>
+                </div>
             </div>
-            <div className="settings">
-                <Settings />
-            </div>
-            <div className="power">
-                <Power />
-            </div>
-        </div>
     </div>
 )}
 
